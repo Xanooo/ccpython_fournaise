498fe2bc0bb752ece73fd310698691ea93392691
Xanooo <75861904+Xanooo@users.noreply.github.com>
2020-12-12 16:37:28 +0100

    Add files via upload

---
+++ ccpython_fournaise.py
@@ -0,0 +1,24 @@
+with open("messagePourSauverLeMonde.txt", "r") as fichier :
+    filin=fichier.read()
+#mot=["volcan"]
+#i=0
+#for i in range :
+#fichier[i]
+print(filin)
+
+
+mot=["volcan"]
+#i=0
+for i in range (80):
+ print(filin[i])
+
+    if filin=="v"
+    filin[+1]=="o"
+    filin[+2]=="l"
+    filin[+3]=="c"
+    filin[+4]=="a"
+    filin[+5]=="n"
+
+
+
+
